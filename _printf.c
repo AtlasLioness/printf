c95c629e25054541b32585d976e500eb42e0ed2f
Maddouchhamza <maddouch.hamza@gmail.com>
2023-04-24 09:20:04 -0700

    Fixing bugs

--- _printf.c
+++ _printf.c
@@ -9,9 +9,11 @@
 int _printf(const char *format, ...)
 {
 	va_list print;
-	int i = 0, j;
+	int i = 0, j, counter = 0;
 	char arg, *str;
 
+	if (format == NULL)
+		return (-1);
 	va_start(print, format);
 	if (format)
 	{
@@ -25,6 +27,7 @@ int _printf(const char *format, ...)
 						arg = va_arg(print, int);
 						write(1, &arg, 1);
 						i = (i + 1) + 1;
+						counter++;
 						break;
 					case 's':
 						j = 0;
@@ -33,8 +36,11 @@ int _printf(const char *format, ...)
 							j++;
 						write(1, str, j);
 						i = i + 2;
+						counter += j - 1;
+						break;
 					default:
 						write(1, format + i, 1);
+						counter++;
 						i++;
 						break;
 				}
@@ -42,10 +48,11 @@ int _printf(const char *format, ...)
 			else
 			{
 				write(1, format + i, 1);
+				counter++;
 				i++;
 			}
 		}
 	}
 	va_end(print);
-	return (0);
+	return (counter);
 }
