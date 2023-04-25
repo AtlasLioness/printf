c4cd64a4ed81d3adbc07edf65288c0fd7dafd05e
AtlasLioness <narjisaliane@gmail.com>
2023-04-25 17:26:57 +0000

    Print b

--- p_functions.c
+++ p_functions.c
@@ -112,11 +112,10 @@ int print_b(va_list print)
 
 	number = va_arg(print, int);
 	fct_rec(number);
-	while (number / 10 > 0)
+	while (number > 0)
 	{
-		number = number / 10;
+		number = number / 2;
 		counter++;
 	}
-	counter++;
 	return (counter);
 }
