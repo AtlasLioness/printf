683d6af2ba07e3956164bfda23823a9306e3752c
Maddouchhamza <maddouch.hamza@gmail.com>
2023-04-25 11:21:28 -0700

    task 3

--- fct_rec.c
+++ fct_rec.c
@@ -1,35 +1,35 @@
 #include "main.h"
 /**
- * fct_rec - convert to binary
- * @a: deciaml to convert
- *
- * Return: none
- */
+* fct_rec - convert to binary
+* @a: deciaml to convert
+*
+* Return: none
+*/
 void fct_rec(unsigned int a)
 {
-    int reste;
+int reste;
 
-    if(a > 0)
-    {
-        reste = a % 2;
-        fct_rec(a / 2);
-        _putchar(reste + '0');
-    }
+if (a > 0)
+{
+reste = a % 2;
+fct_rec(a / 2);
+_putchar(reste + '0');
+}
 }
 /**
- * fct_rec_O - convert to octal
- * @a: deciaml to convert
- *
- * Return: none
- */
+* fct_rec_O - convert to octal
+* @a: deciaml to convert
+*
+* Return: none
+*/
 void fct_rec_O(unsigned int a)
 {
-	int reste;
-	
-       	if(a > 0)
-	{
-		reste = a % 8;
-		fct_rec_O(a / 8);
-		_putchar(reste + '0');
-	}
+int reste;
+
+if (a > 0)
+{
+reste = a % 8;
+fct_rec_O(a / 8);
+_putchar(reste + '0');
+}
 }
